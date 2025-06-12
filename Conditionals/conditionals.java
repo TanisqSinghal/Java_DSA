6eb638d3f8a4ee644f2beefbe451613fcad4aa24
tanishq singhal <you@example.com>
2025-06-12 15:19:32 +0530

    added today's code

---
+++ Conditionals/conditionals.java
@@ -0,0 +1,11 @@
+public class conditionals {
+    public static void main(String args[]) {
+        int num = 2 ;
+        if(num % 2 == 0) {
+            System.err.println("even");
+        }
+        else {
+            System.err.println("odd");
+        }
+    }
+}
