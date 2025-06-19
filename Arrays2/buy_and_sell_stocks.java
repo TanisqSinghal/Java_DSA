359d05f357f6458293e361c40c47124097d03294
tanishq singhal <tanishaksinghal785@gmail.com>
2025-06-19 20:03:48 +0530

    arrays questions

--- Arrays2/buy_and_sell_stocks.java
+++ Arrays2/buy_and_sell_stocks.java
@@ -3,8 +3,8 @@ public class buy_and_sell_stocks {
     public static int bestTimeOfbuyAndsellstocks(int arr[]) {
         int buyPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        for(int i=0; i<arr.length; i++) {
-            if(buyPrice < arr[i]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (buyPrice < arr[i]) {
                 int Profit = arr[i] - buyPrice;
                 maxProfit = Math.max(maxProfit, Profit);
             } else {
@@ -15,7 +15,7 @@ public class buy_and_sell_stocks {
     }
 
     public static void main(String args[]) {
-        int arr[] = {7,1,5,3,6,4};
+        int arr[] = { 7, 1, 5, 3, 6, 4 };
         System.err.println(bestTimeOfbuyAndsellstocks(arr));
     }
 }
