d761abb665fb14dc6a2afa5ae97249a024acbb95
tanishq singhal <tanishaksinghal785@gmail.com>
2025-06-20 12:04:41 +0530

    sorting

---
+++ Basic_Sorting/selection_sort.java
@@ -0,0 +1,29 @@
+public class selection_sort {
+
+    public static void selectionSort(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        selectionSort(arr);
+        print(arr);
+    }
+}
