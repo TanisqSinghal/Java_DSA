11ff7e4f4e8842e7278acc2d2df81d7d9913262e
tanishq singhal <tanishaksinghal785@gmail.com>
2025-06-20 16:46:00 +0530

    sorting

---
+++ Basic_Sorting/counting_sort.java
@@ -0,0 +1,31 @@
+public class counting_sort {
+
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest+1];
+        for(int i=0; i<arr.length; i++) {
+            count[arr[i]]++;
+        }
+        int j=0;
+        for(int i=0; i<count.length; i++) {
+            while(count[i] > 0) {
+                arr[j++] = i;
+                count[i]--;
+            }
+        }
+    }
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5,4,2,3,1};
+        countingSort(arr);
+        print(arr);
+    }
+}
