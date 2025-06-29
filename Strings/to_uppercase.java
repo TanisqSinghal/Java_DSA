e80f2b2527df2b593b5def321e2ecc31514b3ead
tanishq singhal <tanishaksinghal785@gmail.com>
2025-06-29 22:03:50 +0530

    strings modified

--- Strings/to_uppercase.java
+++ Strings/to_uppercase.java
@@ -1,19 +1,21 @@
 public class to_uppercase {
 
-    public static String toUpperCase(String str) {
+    public static StringBuilder UpperCase(String str) {
         int n = str.length();
+        StringBuilder sb = new StringBuilder("");
         for(int i=0; i<n; i++) {
             char ch = str.charAt(i);
-            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
-                continue;
-            } else {
-                str.charAt(i) = ch - 'a' + 'A';
+            if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                ch = (char)(ch - 'a' + 'A');
             }
+            sb.append(ch);
         }
+        return sb;
     }
 
     public static void main(String args[]) {
         String str = "hi, i am tanishak";
-    
+        System.out.println(UpperCase(str));
+        System.out.println(str.toUpperCase());
     }
 }
