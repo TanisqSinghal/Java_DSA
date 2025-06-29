4f71066d354cbdc27e753a5e3f2dced694ae5fbf
tanishq singhal <tanishaksinghal785@gmail.com>
2025-06-29 22:53:13 +0530

    strings modified

--- Strings/to_uppercase.java
+++ Strings/to_uppercase.java
@@ -1,21 +1,40 @@
 public class to_uppercase {
 
-    public static StringBuilder UpperCase(String str) {
-        int n = str.length();
+    // public static StringBuilder UpperCase(String str) {
+    // int n = str.length();
+    // StringBuilder sb = new StringBuilder("");
+    // for(int i=0; i<n; i++) {
+    // char ch = str.charAt(i);
+    // if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+    // ch = (char)(ch - 'a' + 'A');
+    // }
+    // sb.append(ch);
+    // }
+    // return sb;
+    // }
+    public static String capitalize(String str) {
         StringBuilder sb = new StringBuilder("");
-        for(int i=0; i<n; i++) {
+
+        boolean capitalizeNext = true;
+
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
-                ch = (char)(ch - 'a' + 'A');
+            if (ch == ' ') {
+                sb.append(ch);
+                capitalizeNext = true; // next non-space should be capitalized
+            } else if (capitalizeNext) {
+                sb.append(Character.toUpperCase(ch));
+                capitalizeNext = false;
+            } else {
+                sb.append(ch);
             }
-            sb.append(ch);
         }
-        return sb;
+
+        return sb.toString();
     }
 
     public static void main(String args[]) {
-        String str = "hi, i am tanishak";
-        System.out.println(UpperCase(str));
-        System.out.println(str.toUpperCase());
+        String str = " hi, i am tanishak";
+        System.out.println(capitalize(str));
     }
 }
