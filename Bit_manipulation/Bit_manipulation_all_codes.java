5ba5bee8e91e49f1ccd50f797621423ee9aef62c
tanishq singhal <tanishaksinghal785@gmail.com>
2025-07-11 16:32:05 +0530

    bit manipulation

--- Bit_manipulation/Bit_manipulation_all_codes.java
+++ Bit_manipulation/Bit_manipulation_all_codes.java
@@ -33,12 +33,51 @@ public class Bit_manipulation_all_codes {
         int bitMask = -1 << i;
         return n & bitMask;
     }
+    public static int clearIthbitsInrange(int n, int i, int j) {
+        int a = -1 << j+1;
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+    public static boolean isPowerOfTwo(int n) {
+        return (n & (n-1)) == 0;
+    }
+    public static int countSetBits(int n) {
+        int count = 0;
+        while(n > 0) {
+            if((n & 1) == 1) {
+                count++;
+            }
+           n = n >> 1;
+        }
+        return count;
+    }
+    public static int fastExpo(int a, int n) {
+        int ans = 1;
+
+        while(n > 0) {
+            if((n & 1) != 0) {
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
+    }
+    // public static int fastExpo(int n, int b) {
+    //     return n;
+    // }
     public static void main(String args[]) {
-        // oddOreven(6);
-        // System.out.println(getIthbit(15, 2));
-        // System.out.println(setIthbit(15, 4));
-        // System.out.println(clearIthbit(10, 1));
-        // System.out.println(updateIthbit(10, 2, 1));
+        oddOreven(6);
+        System.out.println(getIthbit(15, 2));
+        System.out.println(setIthbit(15, 4));
+        System.out.println(clearIthbit(10, 1));
+        System.out.println(updateIthbit(10, 2, 1));
         System.out.println(clearLastIthbits(15, 2));
+        System.out.println(clearIthbitsInrange(10, 2, 4));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(countSetBits(10));
+        System.out.println(fastExpo(5, 3));
+        System.out.println(fastExpo(5, 3));
     }
 }
