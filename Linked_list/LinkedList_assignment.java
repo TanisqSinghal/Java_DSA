f52a165a36cc5cca92df70545ee718cf924fe980
tanishq singhal <tanishaksinghal785@gmail.com>
2025-07-24 17:25:24 +0530

    added linked list assignment except question 5

--- Linked_list/LinkedList_assignment.java
+++ Linked_list/LinkedList_assignment.java
@@ -8,6 +8,7 @@ public class LinkedList_assignment {
             this.next = null;
         }
     }
+
     public static node head;
     public static node head1;
     public static node head2;
@@ -17,7 +18,7 @@ public class LinkedList_assignment {
     public void addLast(int data) {
         node newNode = new node(data);
         size++;
-        if(head == null) {
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -27,8 +28,8 @@ public class LinkedList_assignment {
 
     public void print(node head) {
         node temp = head;
-        while(temp != null) {
-            System.out.print(temp.data+"->");
+        while (temp != null) {
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
         System.out.println("null");
@@ -37,10 +38,10 @@ public class LinkedList_assignment {
     public static int findLink() {
         node temp1 = head1;
 
-        while(temp1 != null) {
+        while (temp1 != null) {
             node temp2 = head2;
-            while(temp2 != null) {
-                if(temp1.data == temp2.data) {
+            while (temp2 != null) {
+                if (temp1.data == temp2.data) {
                     return temp1.data;
                 }
                 temp2 = temp2.next;
@@ -53,15 +54,15 @@ public class LinkedList_assignment {
     public void skipMdeleteN(int m, int n) {
         node curr = head;
         int count;
-        while(curr != null) {
-            for(count = 1; count < m && curr != null; count++) {
+        while (curr != null) {
+            for (count = 1; count < m && curr != null; count++) {
                 curr = curr.next;
             }
-            if(curr == null) {
+            if (curr == null) {
                 return;
             }
             node t = curr.next;
-            for(count = 1; count < n && t != null; count++) {
+            for (count = 1; count < n && t != null; count++) {
                 t = t.next;
             }
             curr.next = t.next;
@@ -69,6 +70,105 @@ public class LinkedList_assignment {
         }
     }
 
+    // public void swapNodesInLL(int x, int y) {
+    //     node temp1 = head;
+    //     node temp2 = head;
+    //     for (int count = 1; count < x - 1; count++) {
+    //         temp1 = temp1.next;
+    //     }
+    //     for (int count = 1; count < y; count++) {
+    //         temp2 = temp2.next;
+    //     }
+    //     node temp4 = temp1.next;
+    //     node temp3 = temp2.next;
+    //     temp1.next = null;
+    //     temp2.next = null;
+
+    //     node curr = temp4;
+    //     node prev = null;
+    //     node next;
+    //     while (curr != null) {
+    //         next = curr.next;
+    //         curr.next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+    //     temp1.next = prev;
+    //     temp4.next = temp3;
+    // }
+
+    public void swapNodesInLL(int x, int y) {
+        if (x == y)
+            return;
+
+        node prevX = null, currX = head;
+        while (currX != null && currX.data != x) {
+            prevX = currX;
+            currX = currX.next;
+        }
+
+        node prevY = null, currY = head;
+        while (currY != null && currY.data != y) {
+            prevY = currY;
+            currY = currY.next;
+        }
+
+        // If either x or y is not present
+        if (currX == null || currY == null)
+            return;
+
+        // If x is not head of list
+        if (prevX != null)
+            prevX.next = currY;
+        else
+            head = currY;
+
+        // If y is not head of list
+        if (prevY != null)
+            prevY.next = currX;
+        else
+            head = currX;
+
+        // Swap next pointers
+        node temp = currX.next;
+        currX.next = currY.next;
+        currY.next = temp;
+    }
+
+    public node segragateOddOrEven() {
+        if(head == null) return null;
+
+        node evenhead = null; node oddhead = null;
+        node eventail = null; node oddtail = null;
+        node curr = head;
+        while(curr != null) {
+            if(curr.data % 2 == 0) {
+                if(evenhead == null) {
+                    evenhead = eventail = curr;
+                } else {
+                    eventail.next = curr;
+                    eventail = eventail.next;
+                }
+            } else {
+                if(oddhead == null) {
+                    oddhead = oddtail = curr;
+                } else {
+                    oddtail.next = curr;
+                    oddtail = oddtail.next;
+                }
+            }
+            curr = curr.next;
+        }
+
+        if(eventail != null) {
+            eventail.next = oddhead;
+        } 
+        if(oddtail != null) {
+            oddtail.next = null;
+        }
+
+        return evenhead != null ? evenhead : oddhead;
+    }
 
     public static void main(String args[]) {
         // head1 = new node(1);
@@ -85,16 +185,23 @@ public class LinkedList_assignment {
         // print(head2);
         // System.out.println(findLink());
         LinkedList_assignment ll = new LinkedList_assignment();
+        ll.addLast(0);
         ll.addLast(1);
         ll.addLast(2);
         ll.addLast(3);
         ll.addLast(4);
         ll.addLast(5);
         ll.addLast(6);
-        ll.addLast(7);
-        ll.addLast(8);
         ll.print(head);
-        ll.skipMdeleteN(2, 2);
+        // ll.swapNodesInLL(2, 4);
+        ll.segragateOddOrEven();
         ll.print(head);
+        // ll.addLast(5);
+        // ll.addLast(6);
+        // ll.addLast(7);
+        // ll.addLast(8);
+        // ll.print(head);
+        // ll.skipMdeleteN(2, 2);
+        // ll.print(head);
     }
 }
