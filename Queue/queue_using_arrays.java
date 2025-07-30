728f79c51d56e7ecbd71e3e0337094fdca96c4d2
tanishq singhal <tanishaksinghal785@gmail.com>
2025-07-31 00:54:49 +0530

    Queue started

---
+++ Queue/queue_using_arrays.java
@@ -0,0 +1,13 @@
+public class queue_using_arrays {
+    static class Queue {
+        static int arr[];
+        static int size;
+        static int rear;
+
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            
+        }
+    }
+}
