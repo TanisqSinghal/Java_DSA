88122dd1d7bbfae2dbf8bdb8e22666750c578c45
tanishq singhal <tanishaksinghal785@gmail.com>
2025-07-31 13:51:32 +0530

    more queue and Circular queue

--- Queue/circular_queue.java
+++ Queue/circular_queue.java
@@ -1,25 +1,34 @@
-public class queue_using_arrays {
+public class circular_queue {
     static class Queue {
         static int arr[];
         static int size;
+        static int front;
         static int rear;
 
         Queue(int n) {
             arr = new int[n];
             size = n;
-            rear = -1;
+            front = rear = -1;
         }
 
         public static boolean isEmpty() {
-            return rear == -1;
+            return front == -1 && rear == -1;
+        }
+
+        public static boolean isFull() {
+            return (rear + 1) % size == front;
         }
 
         public static void add(int data) {
-            if (rear == size - 1) {
+            if (isFull()) {
                 System.out.println("Queue is full");
                 return;
             }
-            rear = rear + 1;
+            //adding first time 
+            if(front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
             arr[rear] = data;
         }
 
@@ -27,19 +36,20 @@ public class queue_using_arrays {
             if (isEmpty()) {
                 return -1;
             }
-            int front = arr[0];
-            for (int i = 0; i < rear; i++) {
-                arr[i] = arr[i + 1];
+            int first = arr[front];
+            if(front == rear) {
+                front = rear = -1;
+            } else {
+                front = (front + 1) % size;
             }
-            rear = rear - 1;
-            return front;
+            return first;
         }
 
         public static int peek() {
             if (isEmpty()) {
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
     }
 
