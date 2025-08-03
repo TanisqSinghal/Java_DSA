36fc212024a8f0518d414e3b1b457835adbc6d9e
tanishq singhal <tanishaksinghal785@gmail.com>
2025-08-03 20:33:15 +0530

    Greedy more

--- Greedy_algorithms/job_sequencing_problem_self.java
+++ Greedy_algorithms/job_sequencing_problem_self.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class job_sequencing_problem {
+public class job_sequencing_problem_self {
     public static void main(String[] args) {
         int jobs[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
 
