d0cf9649ba21492b9c3354755dfbc3f222f8d3b7
tanishq singhal <tanishaksinghal785@gmail.com>
2025-08-13 23:31:04 +0530

    BST started

---
+++ Binary_search_trees/BST.java
@@ -0,0 +1,46 @@
+public class BST {
+
+    public static class node {
+        int data;
+        node left;
+        node right;
+
+        public node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static node insert(int val, node root) {
+        if(root == null) {
+            node newNode = new node(val);
+            root = newNode;
+            return root;
+        }
+
+        if(root.data > val) {
+            root.left = insert(val, root.left);
+        } else {
+            root.right = insert(val, root.right);
+        }
+
+        return root;
+    }
+
+    public static void inOrder(node root) {
+        if(root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {5, 1, 3, 4, 2, 7};
+        node root = null;
+        for(int i=0; i<nodes.length; i++) {
+            root = insert(nodes[i], root);
+        }
+        inOrder(root);
+    }
+}
