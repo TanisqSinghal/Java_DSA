506dfb2e0ed79a65cb423a1c517b5cf3453fe7c5
tanishq singhal <tanishaksinghal785@gmail.com>
2025-10-30 19:54:15 +0530

    HashMap Intro

---
+++ HashMap/HashMap_intro.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class HashMap_intro {
+    public static void main(String[] args) {
+        //create a hashmap
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        //insert the element - O(1)
+        hm.put("India", 100);
+        hm.put("US", 50);
+        hm.put("China", 150);
+
+        System.out.println(hm);
+
+        //get - O(1)
+        
+    }
+}
