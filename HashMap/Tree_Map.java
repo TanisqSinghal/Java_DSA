f523b49d72d344ecf6ee60e590300f273268b21a
tanishq singhal <tanishaksinghal785@gmail.com>
2025-10-31 22:18:01 +0530

    Linked hashmap and treeMap

---
+++ HashMap/Tree_Map.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Tree_Map {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> tm = new TreeMap<>();
+
+        tm.put("India", 100);
+        tm.put("US", 50);
+        tm.put("China", 150);
+
+        System.out.println(tm);
+    }
+}
