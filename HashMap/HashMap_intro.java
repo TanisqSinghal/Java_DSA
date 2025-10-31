36bfd87365d35811aefc29e3ec4439706daafc3b
tanishq singhal <tanishaksinghal785@gmail.com>
2025-10-31 17:43:10 +0530

    hashmap implementation

--- HashMap/HashMap_intro.java
+++ HashMap/HashMap_intro.java
@@ -13,6 +13,26 @@ public class HashMap_intro {
         System.out.println(hm);
 
         //get - O(1)
-        
+        System.out.println(hm.get("India"));
+        System.out.println(hm.get("Indonesia"));
+
+        //containsKey = O(1)
+        System.out.println(hm.containsKey("India"));
+        System.out.println(hm.containsKey("Indonesia"));
+
+        //removes() - O(1)
+        System.out.println(hm.remove("India"));
+        System.out.println(hm.remove("Nepal"));
+
+        //Size function 
+        System.out.println(hm.size());
+
+        //isEmpty() 
+        System.out.println(hm.isEmpty());
+
+        //clear function
+        hm.clear();
+        System.out.println(hm.size());
+        System.out.println(hm.isEmpty());
     }
 }
