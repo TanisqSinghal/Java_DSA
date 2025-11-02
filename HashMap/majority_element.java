577d9138af2d5833724c176ab3c2cfc5bb8cf6a8
tanishq singhal <tanishaksinghal785@gmail.com>
2025-11-02 12:17:33 +0530

    Hashmap questions

---
+++ HashMap/majority_element.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class majority_element {
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            // if (hm.containsKey(arr[i])) {
+            //     hm.put(arr[i], hm.get(arr[i]) + 1);
+            // } else {
+            //     hm.put(arr[i], 1);
+            // }
+            
+            //OR
+
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (int key : hm.keySet()) {
+            if (hm.get(key) > arr.length / 3) {
+                System.out.print(key + " ");
+            }
+        }
+    }
+}
