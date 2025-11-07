faeb4badd5d7b38634499711f0f775498fd7a13f
tanishq singhal <tanishaksinghal785@gmail.com>
2025-11-07 17:51:24 +0530

    Linked hashset

--- HashMap/HashSets.java
+++ HashMap/HashSets.java
@@ -2,17 +2,39 @@ import java.util.*;
 
 public class HashSets {
     public static void main(String[] args) {
-        HashSet<Integer> hs = new HashSet<>();
+        HashSet<String> hs = new HashSet<>();
 
-        hs.add(1);
-        hs.add(2);
-        hs.add(4);
-        hs.add(2);
-        hs.add(4);
+        hs.add("Delhi");
+        hs.add("Mumbai");
+        hs.add("Bengluru");
+        hs.add("Telangana");
 
-        System.out.println(hs);
-        hs.clear();
-        System.out.println(hs.size());
+        // System.out.println(hs);
+        // hs.clear();
+        // System.out.println(hs.size());
+
+        // Iterator it = hs.iterator();
+
+        // while(it.hasNext()) {
+        //     System.out.println(it.next());
+        // }
+
+        // for(String key: hs) { // more relatable
+        //     System.out.println(key);
+        // }
+
+        //Linked Hashset
+
+        LinkedHashSet<String> set = new LinkedHashSet<>();
+
+        set.add("Delhi");
+        set.add("Mumbai");
+        set.add("Bengluru");
+        set.add("Noida");
+
+        for(String city : set) {
+            System.out.println(city);
+        }
 
     }
 }
