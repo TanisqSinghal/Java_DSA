229ab6bfe231b4f3077abb84354670f248b4b9b7
tanishq singhal <tanishaksinghal785@gmail.com>
2025-11-19 20:56:07 +0530

    graph more

--- Graphs/bfs_traversal.java
+++ Graphs/bfs_traversal.java
@@ -45,6 +45,21 @@ public class bfs_traversal {
         }
     }
 
+    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean visited[]) {
+        if(src == dest) {
+            return true;
+        }
+        visited[src] = true;
+        for(int i=0; i<graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            if(!visited[e.dest] && hasPath(graph, e.dest, dest, visited)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         int V = 7;
         @SuppressWarnings("unchecked")
@@ -84,7 +99,8 @@ public class bfs_traversal {
         //6 vertex
         graph[6].add(new Edge(6, 5, 1));
         // bfs(graph);
-        dfs(graph, 0, new boolean[graph.length]);
+        // dfs(graph, 0, new boolean[graph.length]);
+        System.out.println(hasPath(graph, 0, 8, new boolean[graph.length]));
 
     }
 }
