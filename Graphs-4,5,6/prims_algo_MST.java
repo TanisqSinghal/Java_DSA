eba4432e2aee592faf426cc2c428e2a62412278d
tanishq singhal <tanishaksinghal785@gmail.com>
2026-01-07 18:12:18 +0530

    graphs more

--- Graphs-4,5,6/prims_algo_MST.java
+++ Graphs-4,5,6/prims_algo_MST.java
@@ -1,3 +1,76 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class prims_algo_MST {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            src = s;
+            dest = d;
+            wt = w;
+        }
+    }
+    static class Pair {
+        int v;
+        int cost;
+
+        public Pair(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+    }
+
+    public static void prims(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            Comparator.comparingInt((Pair p) -> (p.cost))
+        );
+        pq.add(new Pair(0, 0));
+        int finalCost = 0; //total min weight
+        while(!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if(!vis[curr.v]) {
+                vis[curr.v] = true;
+                finalCost += curr.cost;
+
+                for(int i=0; i<graph[curr.v].size(); i++) {
+                    Edge e = graph[curr.v].get(i);
+                    pq.add(new Pair(e.dest, e.wt));
+                }
+            }
+        }
+
+        System.out.println("final cost: "+finalCost);
+    }
     
+    public static void main(String[] args) {
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[4];
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
+
+
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
+
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
+
+        prims(graph);
+    }
 }
+
+//Note - we can make ArrayList of pairs for storing the src, dest, corresponding
+//weights of edges which are parth of MST, but here i=we have only calc min cost
