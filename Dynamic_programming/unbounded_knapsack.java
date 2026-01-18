c075e1641ac5df9ec164fb8be59d6bea91b95b8d
tanishq singhal <tanishaksinghal785@gmail.com>
2026-01-18 20:50:13 +0530

    dynamic programming

---
+++ Dynamic_programming/unbounded_knapsack.java
@@ -0,0 +1,57 @@
+public class unbounded_knapsack {
+    //TC - O(N * W)
+    public static int unboundedknapsackTabulation(int val[], int wt[], int W) {
+        int dp[][] = new int[val.length+1][W+1];
+        for(int i=0; i<dp.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int j=0; j<dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<val.length+1; i++) {
+            for(int j=1; j<W+1; j++) {
+                int v = val[i-1];
+                int w = wt[i-1];
+
+                if(w <= j) {
+                    int incProfit = v + dp[i][j-w]; // this line has a key difference b/w 01 knapsack and unbounded knapsack
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                }
+                else {
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+
+        print(dp); //dp arrat will look like this
+
+        return dp[val.length][W];
+    }
+
+    public static void print(int dp[][]) {
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[0].length; j++) {
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+        int dp[][] = new int[val.length+1][W+1];
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(unboundedknapsackTabulation(val, wt, W));
+    }
+}
