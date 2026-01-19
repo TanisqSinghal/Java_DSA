4b5bdbf2fca52242570cfb4091cef6b540ac5230
tanishq singhal <tanishaksinghal785@gmail.com>
2026-01-19 23:24:01 +0530

    dynamic programming

---
+++ Dynamic_programming/longest_common_subsequence_recursion.java
@@ -0,0 +1,23 @@
+public class longest_common_subsequence_recursion {
+
+    public static int lcsRecursion(String str1, String str2, int i, int j) {
+        if(i < 0 || j < 0) {
+            return 0;
+        }
+
+        if(str1.charAt(i) == str2.charAt(j)) {
+            return lcsRecursion(str1, str2, i-1, j-1) + 1;
+        }
+        else {
+            return Math.max(lcsRecursion(str1, str2, i-1, j) , lcsRecursion(str1, str2, i, j-1));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcde";
+        String str2 = "ace";
+
+        System.out.println(lcsRecursion(str1, str2, str1.length()-1, str2.length()-1));
+    }
+}
