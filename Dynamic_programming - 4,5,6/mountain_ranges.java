8884401123b8817cc03a4c99fa79aa0c0e413a45
tanishq singhal <tanishaksinghal785@gmail.com>
2026-01-22 20:21:11 +0530

    dynamic programming

---
+++ Dynamic_programming - 4,5,6/mountain_ranges.java
@@ -0,0 +1,20 @@
+public class mountain_ranges {
+    public static int mountainRanges(int n) { //O(N^2)
+        int dp[] = new int[n+1];
+        dp[0] = dp[1] = 1;
+
+        for(int i=2; i<=n; i++) { //Ci
+            for(int j=0; j<i; j++) {
+                dp[i] += dp[j] * dp[i-j-1]; //Ci = Cj * Ci-j-1
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+
+        System.out.println(mountainRanges(n));
+    }
+}
