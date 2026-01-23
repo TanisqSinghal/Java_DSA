2c2c972d977350bee1d70e05d3f0f9c73f709752
tanishq singhal <tanishaksinghal785@gmail.com>
2026-01-23 16:34:47 +0530

    dynamic programming

--- Dynamic_programming - 4,5,6/matrix_chain_multiplication_memo.java
+++ Dynamic_programming - 4,5,6/matrix_chain_multiplication_memo.java
@@ -18,7 +18,7 @@ public class matrix_chain_multiplication_memo {
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3};
-        int dp[][] = new int[arr.length+1][arr.length+1];
+        int dp[][] = new int[arr.length][arr.length];
         for(int i=0; i<dp.length; i++) {
             for(int j=0; j<dp[0].length; j++) {
                 dp[i][j] = -1;
